019b08cf46471b493e7a751c91c4e0242dc2a28f
dipikam86 <dipikam86@58956712-02ba-11df-a6f0-ed93ac2c91d0>
2010-01-17 11:08:52 +0000

    Initial Import

---
+++ EAGLWeb/src/com/eagl/exception/EaglException.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package com.eagl.exception;
+
+/**
+ * @author Dipika
+ *
+ */
+public class EaglException extends Exception {
+	private String errorCode;
+	/**
+	 * @return the errorCode
+	 */
+	public String getErrorCode() {
+		return errorCode;
+	}
+	/**
+	 * @param errorCode the errorCode to set
+	 */
+	public void setErrorCode(String errorCode) {
+		this.errorCode = errorCode;
+	}
+	/**
+	 * @return the errorMsg
+	 */
+	public String getErrorMsg() {
+		return errorMsg;
+	}
+	/**
+	 * @param errorMsg the errorMsg to set
+	 */
+	public void setErrorMsg(String errorMsg) {
+		this.errorMsg = errorMsg;
+	}
+	private String errorMsg;
+	
+}
