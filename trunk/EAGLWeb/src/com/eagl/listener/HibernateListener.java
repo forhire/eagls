e3d344746fbce0ab57f1a87ea7de442a7e5706ae
dipikam86 <dipikam86@58956712-02ba-11df-a6f0-ed93ac2c91d0>
2010-01-29 14:27:10 +0000

    Hibernate Connection

---
+++ trunk/EAGLWeb/src/com/eagl/listener/HibernateListener.java
@@ -0,0 +1,20 @@
+package com.eagl.listener;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+public class HibernateListener implements ServletContextListener{
+
+	@Override
+	public void contextDestroyed(ServletContextEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void contextInitialized(ServletContextEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
