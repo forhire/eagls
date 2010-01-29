8ffa910ab7b3ff24d4dee7d1fa8525314b36110a
dipikam86 <dipikam86@58956712-02ba-11df-a6f0-ed93ac2c91d0>
2010-01-29 14:27:21 +0000

    Hibernate Connection

---
+++ EAGLWeb/src/com/eagl/hibernate/bean/Test.java
@@ -0,0 +1,112 @@
+package com.eagl.hibernate.bean;
+
+public class Test {
+	private Integer userId;
+
+	private String firstName;
+	private String lastName;
+	private String loginId;
+	private String password;
+	private String emailId;
+
+	public Test() {
+
+	}
+
+	/**
+	 * @return the userId
+	 */
+	public Integer getUserId() {
+		return userId;
+	}
+
+	/**
+	 * @param userId the userId to set
+	 */
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+
+	/**
+	 * @return the firstName
+	 */
+	public String getFirstName() {
+		return firstName;
+	}
+
+	/**
+	 * @param firstName the firstName to set
+	 */
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	/**
+	 * @return the lastName
+	 */
+	public String getLastName() {
+		return lastName;
+	}
+
+	/**
+	 * @param lastName the lastName to set
+	 */
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	/**
+	 * @return the loginId
+	 */
+	public String getLoginId() {
+		return loginId;
+	}
+
+	/**
+	 * @param loginId the loginId to set
+	 */
+	public void setLoginId(String loginId) {
+		this.loginId = loginId;
+	}
+
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+
+	/**
+	 * @param password the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	/**
+	 * @return the emailId
+	 */
+	public String getEmailId() {
+		return emailId;
+	}
+
+	/**
+	 * @param emailId the emailId to set
+	 */
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Test [emailId=" + emailId + ", firstName=" + firstName
+				+ ", lastName=" + lastName + ", loginId=" + loginId
+				+ ", password=" + password + ", userId=" + userId + "]";
+	}
+
+	
+	
+}
